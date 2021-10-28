ef807123da2ca3856d9b61d522ff7bb0edb00136
beltro39 <sbeltrana@unal.edu.co>
2021-10-28 15:57:16 -0500

    Adding describes

--- test/buy-tshirt.spec.ts
+++ test/buy-tshirt.spec.ts
@@ -1,7 +1,9 @@
 import { $, browser } from 'protractor';
-import { ProductListPage, MenuContentPage, ProductAddedModalPage,
-SummaryStepPage, SignInStepPage, AddressStepPage,
-ShippingStepPage, PaymentStepPage, BankPaymentPage } from '../src/page';
+import {
+  ProductListPage, MenuContentPage, ProductAddedModalPage,
+  SummaryStepPage, SignInStepPage, AddressStepPage,
+  ShippingStepPage, PaymentStepPage, BankPaymentPage
+} from '../src/page';
 
 describe('Buy a t-shirt', () => {
   const menuContentPage: MenuContentPage = new MenuContentPage();
@@ -13,23 +15,61 @@ describe('Buy a t-shirt', () => {
   const shippingStepPage: ShippingStepPage = new ShippingStepPage();
   const paymentStepPage: PaymentStepPage = new PaymentStepPage();
   const bankPaymentPage: BankPaymentPage = new BankPaymentPage();
+  describe('Open the page in the browser', () => {
 
-  it('then should be bought a t-shirt', async () => {
-    await browser.get('http://automationpractice.com/');
-    await menuContentPage.goToTShirtMenu();
-    await productListPage.goToAddToCartButton();
-    await(browser.sleep(3000));
-    await productAddedModalPage.goToProceedToCheckoutButton();
-    await summaryStepPage.goToProceedToCheckoutButton();
-    await signInStepPage.putEmailAddress();
-    await signInStepPage.putPassword();
-    await signInStepPage.goToSignInButton();
-    await addressStepPage.goToProceedToCheckoutButton();
-    await shippingStepPage.goToTermsOfServiceCheckBox();
-    await shippingStepPage.goToProceedToCheckoutButton();
-    await paymentStepPage.goToPayByBankWireButton();
-    await bankPaymentPage.goToConfirmMyOrderButton();
-    await expect($('#center_column > div > p > strong').getText())
-      .toBe('Your order on My Store is complete.');
+    it('then the page should be loaded', async () => {
+      await browser.get('http://automationpractice.com/');
+    });
+  });
+
+  describe('Buying a TShirt process', () => {
+    it('then page should be in the t-shirt section', async () => {
+      await menuContentPage.goToTShirtMenu();
+    });
+    it('then the t-shirt should be added to the cart', async () => {
+      await productListPage.goToAddToCartButton();
+      await (browser.sleep(3000));
+    });
+    it('then proceed to checkout the cart', async () => {
+      await productAddedModalPage.goToProceedToCheckoutButton();
+    });
+    it('then proceed to checkout the t-shirt (summary step)', async () => {
+      await summaryStepPage.goToProceedToCheckoutButton();
+    });
+  });
+
+  describe('Login in the application', () => {
+    it('then the email address should have been typed', async () => {
+      await signInStepPage.putEmailAddress();
+    });
+    it('then the password should have been typed', async () => {
+      await signInStepPage.putPassword();
+    });
+    it('then should log in the application', async () => {
+      await signInStepPage.goToSignInButton();
+    });
+  });
+  describe('Select the default address', () => {
+    it('then should proceed to checkout (address step)', async () => {
+      await addressStepPage.goToProceedToCheckoutButton();
+    });
+    it('then should acept the terms of service', async () => {
+      await shippingStepPage.goToTermsOfServiceCheckBox();
+    });
+    it('then should proceed to checkout (shipping step)', async () => {
+      await shippingStepPage.goToProceedToCheckoutButton();
+    });
+  });
+  describe('Payment at the bank', () => {
+    it('then should have selected payment method', async () => {
+      await paymentStepPage.goToPayByBankWireButton();
+    });
+    it('then should have confirmed order', async () => {
+      await bankPaymentPage.goToConfirmMyOrderButton();
+    });
+    it('then the order should be complete', async () => {
+      await expect($('#center_column > div > p > strong').getText())
+            .toBe('Your order on My Store is complete.');
+    });
   });
 });
